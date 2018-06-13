75b67648a375367fb34f00d771aef4202fbf1902
Ali-Rathore <alirthr@gmail.com>
2018-06-13 14:49:17 +0500

    blan activity is being forwarded to you mate

--- app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
+++ app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
@@ -10,53 +10,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // hi ali if you select update you directly get the latest version from the github that i pushed
-    // so what if you want to compare the alterations of the other developers??
-    // compare and see which ones you want to include in your project?
-    // ask me when you read this
-
-    // for comparing and merging
-    // see if you see this!!!!
-
-    LinearLayout main_ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // finding the view in the MainActivity xml
-        main_ll = findViewById(R.id.ll_main);
-
-        // calling the function for showing up the TextView dynamically
-        createTextView();
-        createTextView();
-        createTextView();
-        createTextView();
-        createTextView();
-        createTextView();
-
-
     }
-
-    public void createTextView()
-    {
-        // declaring and initialising a TextView
-        TextView tv = new TextView(this);
-
-        // deciding layout parameters
-        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        // setting the layout parameters to the TextView
-        tv.setLayoutParams(lp);
-
-        // setting text in the TextView
-        tv.setText("Hi tesing this thing");
-
-        main_ll.addView(tv);
-    }
-
 }
-
-
-;
