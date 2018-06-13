12bd36569e6a065858cf13b75df1cad063125f3e
AmmarSaqib <ammarsaqib123@gmail.com>
2018-06-13 15:53:08 +0500

    setting up the fragment
    Class needed to be constructed completely

--- app/src/main/java/com/example/ammarsaqib/docapp/FragmentOne.java
+++ app/src/main/java/com/example/ammarsaqib/docapp/FragmentOne.java
@@ -3,12 +3,36 @@ package com.example.ammarsaqib.docapp;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class FragmentOne extends Fragment {
-    
+
+    Button btn_1, btn_2, btn_3, btn_3;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // you create a view variable to inflate the fragment xml
+            //
+            //
+
+        // button iniitalising here
+        /*
+            btn_1 = view.findViewById();
+            listerners on buttons
+         */
+
+        // https://github.com/mitchtabian/Fragments/blob/master/Fragements/app/src/main/java/tabian/com/fragements/Fragment1.java
+
+        //        return super.onCreateView(inflater, container, savedInstanceState);
+
+        // return view;
+    }
 }
