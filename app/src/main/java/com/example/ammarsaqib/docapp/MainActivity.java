43a97cb058bdec1eeda88fa6f24c55be4c08df88
Ali-Rathore <alirthr@gmail.com>
2018-06-13 16:45:10 +0500

    Fragment for home page buttons with listeners added here in fragment one added fragment manager and a commit in main

--- app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
+++ app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.ammarsaqib.docapp;
 
-import android.app.ActionBar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Layout;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    LinearLayout mainXml;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainXml = findViewById(R.id.ll_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        FragmentOne home = new FragmentOne();
+        ft.replace(R.id.llmain, home);
+        ft.commit();
+
+
     }
+
+
 }
