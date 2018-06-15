9cea5ea2dcd70b1a30e5f52c729e6971d3407df1
AmmarSaqib <ammarsaqib123@gmail.com>
2018-06-16 02:58:07 +0500

    home screen fragment set navigation drawer working drawer item are clickable and about us is working (dialog fragment)

--- app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
+++ app/src/main/java/com/example/ammarsaqib/docapp/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.ammarsaqib.docapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
+    NavigationView navMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // referencing main navigation view
+        navMain = findViewById(R.id.nav_main);
+        navMain.setNavigationItemSelectedListener(this);
+
+
         // loading home screen fragment
         load_home_frag();
 
         // setting drawer
-        drawerLayout = (DrawerLayout) findViewById(R.id.draw_main);
+        drawerLayout = findViewById(R.id.draw_main);
         toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
@@ -32,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
         if (toggle.onOptionsItemSelected(item))
         {
             return true;
@@ -39,14 +54,79 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     private void load_home_frag()
     {
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction ft = fm.beginTransaction();
         //commit
         FragmentOne home = new FragmentOne();
-        ft.add(R.id.draw_main, home);
+        ft.replace(R.id.fragment_replaceable, home);
+
+        // adding to back stack
+        ft.addToBackStack("home");
         ft.commit();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.d("navigation on click", "function called");
+        Fragment fragment = null;
+        Class fragmentClass;
+
+        String stack = " ";
+
+        switch(item.getItemId()) {
+
+            case R.id.aboutus:
+
+                fragmentClass = null;
+                aboutus frag = new aboutus();
+                frag.show(this.getSupportFragmentManager(),"DialogFragment" );
+                stack = "about_us";
+
+                // toast is showing up on the main activity
+//                Toast.makeText(this, "testing",Toast.LENGTH_LONG).show();
+
+                break;
+
+            default:
+
+                fragmentClass = FragmentOne.class;
+
+        }
+
+
+        if (fragmentClass!=null)
+        {
+            // execute the block if only there is some class assigned to the variable
+            try {
+
+                fragment = (Fragment) fragmentClass.newInstance();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+            // Insert the fragment by replacing any existing fragment
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            ft.replace(R.id.fragment_replaceable, fragment);
+            ft.addToBackStack(stack);
+            ft.commit();
+        }
+        // Highlight the selected item has been done by NavigationView
+        item.setChecked(true);
+
+        // Set action bar title
+        setTitle(item.getTitle());
+
+        // Close the navigation drawer
+        drawerLayout.closeDrawers();
+
+        return true;
+    }
 }
---
+++ app/src/main/java/com/example/ammarsaqib/docapp/aboutus.java
@@ -0,0 +1,48 @@
+package com.example.ammarsaqib.docapp;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link aboutus.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class aboutus extends DialogFragment {
+
+    Button cancel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        // creating the inflater for about us
+        View about_us = inflater.inflate(R.layout.fragment_aboutus,container,false);
+
+        // initialising the button
+        cancel = about_us.findViewById(R.id.aboutus_dismiss);
+        cancel.setOnClickListener(new View.OnClickListener() {
+
+            // setting on click for dismissing the dialog
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+        return about_us;
+    }
+}
